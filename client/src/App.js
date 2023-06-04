98b419e7b61cb0ac3c5a6aba4d8097c52bb70223
Yi Huang <yihuang@Yis-MacBook-Pro.local>
2023-06-03 21:12:58 -0700

    update client

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
+
+function App() {
+  return (
+    <div className="app">
+    <BrowserRouter>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ScrollToTop>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
